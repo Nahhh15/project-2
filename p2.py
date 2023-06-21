79ad7654ed620431e2185dd86f7ca5a4a7f6bd6b
Nahhh15 <126382815+Nahhh15@users.noreply.github.com>
2023-06-21 14:09:14 +0530

    Add files via upload

---
+++ p2.py
@@ -0,0 +1,46 @@
+def add(x, y):
+    return x + y
+def subtract(x, y):
+    return x - y
+def multiply(x, y):
+    return x * y
+def divide(x, y):
+    return x / y
+
+
+print("Select operation to be performed.")
+print("1.Addition")
+print("2.Subtraction")
+print("3.Multiplication")
+print("4.Division")
+
+while True:
+    # take input from the user
+    choice = input("Enter choice(1/2/3/4): ")
+
+    if choice in ('1', '2', '3', '4'):
+        try:
+            num1 = float(input("Enter first number: "))
+            num2 = float(input("Enter second number: "))
+        except ValueError:
+            print("Invalid input. Please enter a number.")
+            continue
+
+        if choice == '1':
+            print(num1, "+", num2, "=", add(num1, num2))
+
+        elif choice == '2':
+            print(num1, "-", num2, "=", subtract(num1, num2))
+
+        elif choice == '3':
+            print(num1, "*", num2, "=", multiply(num1, num2))
+
+        elif choice == '4':
+            print(num1, "/", num2, "=", divide(num1, num2))
+
+
+        next_calculation = input("To Continue operations click 1 ; 4to exit click 2:  ")
+        if next_calculation == "2:":
+          break
+    else:
+        print("Invalid Input")
